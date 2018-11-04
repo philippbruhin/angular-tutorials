df6af31f7d9258e630e5d6ad3db1686a43f6b7bf
philippbruhin <info@philippbruhin.ch>
2018-11-04 23:14:45 +0100

    added databinding start project

--- shopping-list/src/app/shopping-list/shopping-list.component.ts
+++ shopping-list/src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Bananas', 3),
+    new Ingredient('Coconut', 1)
+  ];
 
   constructor() { }
 
